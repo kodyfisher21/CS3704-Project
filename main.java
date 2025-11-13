781f26186f2337be7c1218e41bc75f6972dde3f9
Yash Atal <yashatal@perisimmon.rlogin>
2025-11-13 10:22:40 -0500

    This will ask for user name

--- main.java
+++ main.java
@@ -1,8 +1,18 @@
 public class main {
       public static void main(String[] args) {
         //create an array of students with random times and courses ()
+        array of student students = new array of student[20];
+        array of string courses = {"CS101", "CS102", "CS103", "CS104", "CS105"};
+        array of string times = {"Morning", "Afternoon", "Evening"};
 
         //id only make 4 or 5 courses and only a handful of times. maybe morning, afternoon, and evening 
+
+        public Sting getName(){
+          System.out.println("Enter your name:");
+          String name = scanner.nextLine();
+          return name;
+          
+        }
 
         //then loop through the students and randomly match students with same course and time
 
