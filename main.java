be45dfe52f0a5a298d484f178613f5d68ca56e3e
nicolasgoossens1 <nicolasgoossens7@gmail.com>
2025-11-13 10:15:05 -0500

    this has basic student/course framework as well as pseudocode for the rest

---
+++ main.java
@@ -0,0 +1,14 @@
+public class main {
+      public static void main(String[] args) {
+        //create an array of students with random times and courses ()
+
+        //id only make 4 or 5 courses and only a handful of times. maybe morning, afternoon, and evening 
+
+        //then loop through the students and randomly match students with same course and time
+
+        //then print the pairs of students
+
+        //
+      }
+
+}
