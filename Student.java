a5ccc845712b64d75767d41424421e3be1e5643e
jayfoti96 <jayfoti96@vt.edu>
2025-12-05 15:14:56 -0500

    added friend functionality to student, may need to add use in main

--- Student.java
+++ Student.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /* 
 defines the Student class fields, constructors, and methods
  
@@ -9,17 +11,20 @@ schedule = 2D String array to store the Student's class schedule, first set is
 public class Student {
     private String name;
     private String[][] schedule;
+    private ArrayList<String> friends;
 
     // constructor without a preset schedule, intializes the proper blank array
     public Student(String name) {
         this.name = name;
         this.schedule = new String[5][13];
+        this.friends = new ArrayList<String>();
     }   
 
     // constructor with a preset schedule
-    public Student(String name, String[][] schedule) {
+    public Student(String name, String[][] schedule, ArrayList<String> friends) {
         this.name = name;
         this.schedule = schedule;
+        this.friends = friends;
     }   
 
     // getter for name
@@ -32,6 +37,11 @@ public class Student {
         return schedule;
     }
 
+    public ArrayList<String> getFriends()
+    {
+        return friends;
+    }
+
     // adds class to the schedule on the correct day and time, does not add if invalid
     public void addClass(int day, int hour, int howManyHourBlocks, String className) {
         if(day < 0 || day > 4 || hour < 8 || hour > 19 || howManyHourBlocks < 1 || howManyHourBlocks > 13) {
@@ -41,5 +51,15 @@ public class Student {
         for(int i = 0; i < howManyHourBlocks; i++) {
             schedule[day][hour - 8 + i] = className;
         }
+    }
+
+    public void addFriend(String name)
+    {
+        friends.add(name);
+    }
+
+    public void removeFriend(String name)
+    {
+        friends.remove(name);
     }
 }
