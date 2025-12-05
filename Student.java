9900aff6b0bab262bf50d80426b50bd1f6c97de5
kodyfisher <kodyfisher@vt.edu>
2025-12-05 17:51:11 -0500

    added student bio feature so students can describe themselves - code review

--- Student.java
+++ Student.java
@@ -1,8 +1,3 @@
-
-import java.util.ArrayList;
-
-// friend functionality for code review
-
 /* 
 defines the Student class fields, constructors, and methods
  
@@ -10,17 +5,28 @@ name = name of student as a String
 schedule = 2D String array to store the Student's class schedule, first set is
 [0-4] representing M-F, second set is [0-12] representing 8am to 7pm
  */
+import java.util.ArrayList;
+
+// friend functionality for code review
+
 // gpa functionality for code review
 /*
  deines the gpa field and its getter and setter. that way students can also track
  their academic performance
  */
+
+// student bio functionality for code review
+/*
+    allows students to describe themselves in a bio such as study habits
+    and personal goals
+*/
 public class Student {
 
     private String name;
     private String[][] schedule;
     private ArrayList<String> friends;
     private double gradePointAverage;
+    private String bio;
 
     // constructor without a preset schedule, intializes the proper blank array
     public Student(String name) {
@@ -28,14 +34,16 @@ public class Student {
         this.schedule = new String[5][13];
         this.friends = new ArrayList<String>();
         this.gradePointAverage = 0.0;
+        this.bio = "";
     }
 
     // constructor with a preset schedule
-    public Student(String name, String[][] schedule, ArrayList<String> friends, double gradePointAverage) {
+    public Student(String name, String[][] schedule, ArrayList<String> friends, double gradePointAverage, String bio) {
         this.name = name;
         this.schedule = schedule;
         this.friends = friends;
         this.gradePointAverage = gradePointAverage;
+        this.bio = bio;
     }
 
     // getter for name
@@ -78,5 +86,15 @@ public class Student {
 
     public void setGradePointAverage(double gpa) {
         gradePointAverage = gpa;
+    }
+
+    //getter for the student bio
+    public void getBio() {
+        return bio;
+    }
+
+    //students can set their own bio
+    public void setBio(String description) {
+        bio = description;
     }
 }
